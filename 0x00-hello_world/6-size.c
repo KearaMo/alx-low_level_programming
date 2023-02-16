89b73ea21613c5ba9517f93a644d277502832588
Kearabetsoe Morudu <kearabetsoemorudu@gmail.com>
2023-02-16 14:31:20 -0800

    corrected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of a char: %lu byte(s)", (signed long)sizeof(a));
-printf("Size of an int: %lu byte(s)", (signed long)sizeof(b));
-printf("Size of a long int: %lu byte(s)", (signed long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)", (signed long)sizeof(d));
-printf("Size of a float: %lu byte(s)", (signed long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (signed long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (signed long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (signed long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (signed long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (signed long)sizeof(f));
 return (0);
 }
