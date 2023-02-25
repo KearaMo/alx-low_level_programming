0679c68c4543b2e5f71420ef7139c4a6ed6631cf
Kearabetsoe Morudu <kearabetsoemorudu@gmail.com>
2023-02-25 03:30:39 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for a digit (0 through 9)
+ * @c: number to check
+ * Return: 1 (success), 0 if not
+ */
+
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
