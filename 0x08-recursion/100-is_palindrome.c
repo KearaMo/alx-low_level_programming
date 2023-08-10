461bc4280aea471fb62175f31bd0e9c75a29af08
Kearabetsoe Morudu <kearabetsoemorudu@gmail.com>
2023-08-10 06:45:52 -0700

    the extra exercises

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if string is a palindrome
+ * @s: string to reverse
+ * Return: 1 if string is a palindrome and 0 if not
+ */
+
+int is_palindrome(char *s);
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * check_pal - recurse to check for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of string
+ * Return: 1 if string is a palindrome and 0 if not
+ */
+
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + 1) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
